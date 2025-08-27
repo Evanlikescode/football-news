c2abe492ff3b00437f78fe91dfb41e97ec0b33ff
Shotheaven <evan@Shotheavens-MacBook-Air.local>
2025-08-27 14:36:36 +0700

    add allowed host again

--- football_news/football_news/settings.py
+++ football_news/football_news/settings.py
@@ -36,14 +36,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "https://evan-haryo-footballnews.pbp.cs.ui.ac.id",
-    "https://evan-haryo-footballnews.pbp.cs.ui.ac.id",
-    "evan-haryo-footballnews.pbp.cs.ui.ac.id",
-    
-]
+ALLOWED_HOSTS = ["localhost","127.0.0.1","evan-haryo-footballnews.pbp.cs.ui.ac.id"]
 
 
 
